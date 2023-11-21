9580d7d53cdfb52ca86ff5fadc783647eb9b5e39
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2023-11-21 15:03:23 +0300

    dateparsing start

--- Datasets_Generator/model_test.py
+++ Datasets_Generator/model_test.py
@@ -10,3 +10,5 @@ nlp_ner = spacy.load("model-best")
 sentence = "remind me to catch the bus tomorrow at 13:45"
 
 print(classify_entities(sentence, nlp_ner))
+print(classify_entities("remind me to see the cartoon the day after tomorrow at 5:45", nlp_ner))
+print(classify_entities("remind me to feed cat in 30 minutes", nlp_ner, nlp))
